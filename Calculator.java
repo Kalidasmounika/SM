a2e9155671f23f220cefe8f466a147991f53cc40
mca <mca@L1-26.ssccdomain.in>
2025-10-23 15:45:36 +0530

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+Calculator.java:
+Public class Calculator{
+public init add(int a, int b){return a+b:}
+public init subtract(int a, int b){return a-b:}
+public init multiply(int a, int b){return a*b:}
+public init divide(int a, int b){if (b==0)throw new arithmetic exception("cannot divided by zero"); return a/b:}
+public static void main(string[]args){
+Calculator Calc=new Calculator();
+System.out.println("addition:"+Calc.add(10,5));
+System.out.println("subtraction:"+Calc.subtract(10,5));
+System.out.println("multiplication:"+Calc.multiply(10,5));
+System.out.println("division:"+Calc.divide(10,5));
+}}
