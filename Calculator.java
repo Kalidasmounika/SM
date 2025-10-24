825febad4b84b02cd61a170cde94992752543070
Kalidasmounika <mounikakalidas26@gmail.com>
2025-10-24 14:15:37 +0530

    Correct case for Calculator instance in main method
    Fixed method calls in main to use correct case for 'Calc'.

--- Calculator.java
+++ Calculator.java
@@ -6,8 +6,8 @@ public init multiply(int a, int b){return a*b:}
 public init divide(int a, int b){if (b==0)throw new arithmetic exception("cannot divided by zero"); return a/b:}
 public static void main(String[]args){
 Calculator Calc=new Calculator();
-System.out.println("addition:" +Calc.add(10,5));
-System.out.println("subtraction:" +Calc.subtract(10,5));
-System.out.println("multiplication:" +Calc.multiply(10,5));
-System.out.println("division:" +Calc.divide(10,5));
+System.out.println("addition:" + calc.add(10,5));
+System.out.println("subtraction:" + calc.subtract(10,5));
+System.out.println("multiplication:" + calc.multiply(10,5));
+System.out.println("division:" + calc.divide(10,5));
 }}
